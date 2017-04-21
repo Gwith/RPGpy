9155952161895c51a07f2e135935c2853b33ed71
Gwith <jmacfiggen@gmail.com>
2017-04-21 14:41:52 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -2,20 +2,13 @@ import pygame, sys
 
 from pygame.locals import *
 
-black = (0, 0, 0)
-brown = (153, 76, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
-red = (255, 0, 0)
-grey = (105, 105, 105)
-
 dirt = 0
 grass = 1
 water = 2
 coal = 3
 rock = 4
 lava = 5
-'''
+
 textures = {
     dirt : pygame.image.load('src/textures/dirt.jpg'),
     grass : pygame.image.load('src/textures/grass.png'),
@@ -23,34 +16,29 @@ textures = {
     coal : pygame.image.load('src/textures/coal.jpg'),
     rock : pygame.image.load('src/textures/rock.jpg'),
     lava : pygame.image.load('src/textures/lava.jpg')
-}'''
-
-colors = {
-    blue : (0, 0, 255),
-    green : (0, 255, 0)
 }
 
 tilemap = [
-    [blue, blue, green, green, blue, blue, blue, blue, green, green, blue, blue, blue, blue, blue],
-    [blue, blue, green, green, blue, blue, blue, blue, green, green, blue, blue, blue, blue, blue],
-    [blue, blue, green, green, blue, blue, blue, blue, green, green, blue, blue, blue, blue, blue],
-    [blue, blue, green, green, blue, blue, blue, blue, green, green, blue, blue, blue, blue, blue],
-    [blue, blue, green, green, blue, blue, blue, blue, green, green, blue, blue, blue, blue, blue],
-    [blue, blue, green, green, blue, blue, blue, blue, green, green, blue, blue, blue, blue, blue],
-    [blue, blue, green, green, blue, blue, blue, blue, green, green, blue, blue, blue, blue, blue],
-    [blue, blue, green, green, blue, blue, blue, blue, green, green, blue, blue, blue, blue, blue],
-    [blue, blue, green, green, blue, blue, blue, blue, green, green, blue, blue, blue, blue, blue],
-    [blue, blue, green, green, blue, blue, blue, blue, green, green, blue, blue, blue, blue, blue],
-    [blue, blue, green, green, blue, blue, blue, blue, green, green, blue, blue, blue, blue, blue],
-    [blue, blue, green, green, blue, blue, blue, blue, green, green, blue, blue, blue, blue, blue],
-    [blue, blue, green, green, blue, blue, blue, blue, green, green, blue, blue, blue, blue, blue],
-    [blue, blue, green, green, blue, blue, blue, blue, green, green, blue, blue, blue, blue, blue],
-    [blue, blue, green, green, blue, blue, blue, blue, green, green, blue, blue, blue, blue, blue],
-    [blue, blue, green, green, blue, blue, blue, blue, green, green, blue, blue, blue, blue, blue],
-    [blue, blue, green, green, blue, blue, blue, blue, green, green, blue, blue, blue, blue, blue],
-    [blue, blue, green, green, blue, blue, blue, blue, green, green, blue, blue, blue, blue, blue],
-    [blue, blue, green, green, blue, blue, blue, blue, green, green, blue, blue, blue, blue, blue],
-    [blue, blue, green, green, blue, blue, blue, blue, green, green, blue, blue, blue, blue, blue],
+    [lava, lava, lava, lava, lava, lava, grass, grass, grass, lava, lava, lava, lava, lava, lava],
+    [lava, lava, lava, lava, lava, lava, grass, grass, grass, lava, lava, lava, lava, lava, lava],
+    [lava, lava, lava, lava, lava, lava, grass, grass, grass, lava, lava, lava, lava, lava, lava],
+    [lava, lava, lava, lava, lava, lava, grass, grass, grass, lava, lava, lava, lava, lava, lava],
+    [lava, lava, lava, lava, lava, lava, grass, grass, grass, lava, lava, lava, lava, lava, lava],
+    [lava, lava, lava, lava, lava, lava, grass, grass, grass, lava, lava, lava, lava, lava, lava],
+    [lava, lava, lava, lava, lava, lava, grass, grass, grass, lava, lava, lava, lava, lava, lava],
+    [lava, lava, lava, lava, lava, lava, grass, grass, grass, lava, lava, lava, lava, lava, lava],
+    [lava, lava, lava, lava, lava, lava, grass, grass, grass, lava, lava, lava, lava, lava, lava],
+    [lava, lava, lava, lava, lava, lava, grass, grass, grass, lava, lava, lava, lava, lava, lava],
+    [lava, lava, lava, lava, lava, lava, grass, grass, grass, lava, lava, lava, lava, lava, lava],
+    [lava, lava, lava, lava, lava, lava, grass, grass, grass, lava, lava, lava, lava, lava, lava],
+    [lava, lava, lava, lava, lava, lava, grass, grass, grass, lava, lava, lava, lava, lava, lava],
+    [lava, lava, lava, lava, lava, lava, grass, grass, grass, lava, lava, lava, lava, lava, lava],
+    [lava, lava, lava, lava, lava, lava, grass, grass, grass, lava, lava, lava, lava, lava, lava],
+    [lava, lava, lava, lava, lava, lava, grass, grass, grass, lava, lava, lava, lava, lava, lava],
+    [lava, lava, lava, lava, lava, lava, grass, grass, grass, lava, lava, lava, lava, lava, lava],
+    [lava, lava, lava, lava, lava, lava, grass, grass, grass, lava, lava, lava, lava, lava, lava],
+    [lava, lava, lava, lava, lava, lava, grass, grass, grass, lava, lava, lava, lava, lava, lava],
+    [lava, lava, lava, lava, lava, lava, grass, grass, grass, lava, lava, lava, lava, lava, lava],
 
 ]
 
@@ -71,11 +59,8 @@ while True:
             sys.exit()
     for row in range(mapheight):
         for column in range(mapwidth):
-            pygame.draw.rect(dispalySurf,
-                             colors[tilemap[row][column]],
+            dispalySurf.blit(textures[tilemap[row][column]],
                              (column*tilesize,
-                              row*tilesize,
-                              tilesize,
-                              tilesize))
+                              row*tilesize,))
 
     pygame.display.update()
